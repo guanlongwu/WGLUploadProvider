dbab966e49ca11b3d353cc9ddcfd7ad3afc63e71
guanlongwu <guanlongwu@qq.com>
2019-04-23 20:52:37 +0800

    This is a file upload manager that support file chunks upload

---
+++ WGLUploadProvider/WGLUploadProvider.h
@@ -0,0 +1,13 @@
+//
+//  WGLUploadProvider.h
+//  WGLUploadProvider
+//
+//  Created by wugl on 2019/4/23.
+//  Copyright © 2019 WGLKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WGLUploadProvider : NSObject
+
+@end
---
+++ WGLUploadProvider/WGLUploadProvider.m
@@ -0,0 +1,13 @@
+//
+//  WGLUploadProvider.m
+//  WGLUploadProvider
+//
+//  Created by wugl on 2019/4/23.
+//  Copyright © 2019 WGLKit. All rights reserved.
+//
+
+#import "WGLUploadProvider.h"
+
+@implementation WGLUploadProvider
+
+@end
